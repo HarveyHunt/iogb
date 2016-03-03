3d4472b1a39c770e68addebea18ba32700c6733e
Harvey Hunt <harveyhuntnexus@gmail.com>
2016-03-03 13:06:27 +0000

    Get a rom from the command line and create a Cartridge

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,18 @@
+use std::env::args;
+
 mod gb;
 mod cpu;
 mod mmu;
 mod cartridge;
 
 fn main() {
+    let args: Vec<_> = args().collect();
+
+    if args.len() != 2 {
+        panic!("usage: iogb <rom>");
+    }
+
+    let cart = cartridge::Cartridge::new(&args[1]);
     let gb = gb::GameBoy::new();
 
     println!("gb: {:?}", gb);
